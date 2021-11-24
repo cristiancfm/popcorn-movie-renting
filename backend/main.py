be0447f4aecdf463f4c3cb87d836799f404b7eaf
cristian <cristian.ferreiro@udc.es>
2021-11-24 12:20:13 +0200

    login and signup pages created

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True) #if debugging is activated, changes to the code will rerun the webserver automatically
+    app.run(debug=True) 
+    #if debugging is activated, changes to the code will rerun the webserver automatically
