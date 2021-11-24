f1d1eae16b9b590500c9c68d9b9fc5de64e1ac74
cristian <cristian.ferreiro@udc.es>
2021-11-24 11:36:04 +0200

    backend first steps

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) #if debugging is activated, changes to the code will rerun the webserver automatically
