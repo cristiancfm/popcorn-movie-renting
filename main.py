014e900bad763c22ac8ea1c4f3edfb6b5a6c177d
cristian <cristian.ferreiro@udc.es>
2021-12-05 19:40:07 +0200

    .

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True) 
     # if debugging is activated, changes to the code 
-    # will rerun the webserver automatically
+    # will rerun the webserver automatically 
