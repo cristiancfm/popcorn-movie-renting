d3da0a28b7aef3590bceee5708309b8ad59479de
cristian <cristian.ferreiro@udc.es>
2021-12-05 21:49:34 +0200

    responsive layout for movies page

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
 from flask_login import  login_required, current_user
 
+
 views = Blueprint('views', __name__)
 
 @views.route('/')
-
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/movies')
+def movies():
+    return render_template("movies.html", user=current_user)
