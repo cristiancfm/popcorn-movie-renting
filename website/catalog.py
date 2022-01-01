7c2e3c1174b9a92b4f6ee152c91aef38f3c889ef
cristian <cristian.ferreiro@udc.es>
2022-01-01 22:00:58 +0100

    added movie links

--- website/catalog.py
+++ website/catalog.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from flask_login import  login_required, current_user
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 import tmdbsimple as tmbd
 
 tmbd.API_KEY = 'f2cba5229db26a4e1e3e1a90409d363d'
@@ -7,9 +7,18 @@ tmbd.API_KEY = 'f2cba5229db26a4e1e3e1a90409d363d'
 
 catalog = Blueprint('catalog', __name__)
 
+
 @catalog.route('/movies')
 def movies():
+    return redirect(url_for('catalog.popular_movies'))
 
-    popular_movies = tmbd.Movies().popular()['results']
 
-    return render_template("movies.html", user=current_user, movies=popular_movies)
+@catalog.route('/movies/popular')
+def popular_movies():
+    movies_list = tmbd.Movies().popular()['results']
+    return render_template("movies.html", user=current_user, movies=movies_list)
+
+
+@catalog.route('/movies/<int:movie_id>')    # int: only integer will be passed in the url otherwise it will give a 404 error
+def movie(movie_id):
+    return ('you asked for movie{0}'.format(id))
